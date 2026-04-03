5dc85131e9adf6778a3ad76bbfcc8edffd831f25
João Inez <joaoinez.info@gmail.com>
2026-04-03 16:50:56 +0200

    feat: llama cpp script

---
+++ scripts/serve-llm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export LLAMA_CACHE="unsloth/Qwen3.5-9B-GGUF"
+llama-server \
+  -hf unsloth/Qwen3.5-9B-GGUF:UD-Q6_K_XL \
+  --ctx-size 16384 \
+  --temp 1.0 \
+  --top-p 0.95 \
+  --top-k 20 \
+  --min-p 0.00 \
+  --alias "unsloth/Qwen3.5-9B-GGUF" \
+  --port 8001 \
+  --chat-template-kwargs '{"enable_thinking":true}'
