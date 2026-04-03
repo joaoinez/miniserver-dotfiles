274d1bb96c0141d22b4656e0cbbf56a138997cbd
João Inez <joaoinez.info@gmail.com>
2026-04-03 17:48:56 +0200

    refactor: change small model

--- scripts/serve-small-llm.sh
+++ scripts/serve-small-llm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export LLAMA_CACHE="unsloth/Qwen3.5-2B-GGUF"
+export LLAMA_CACHE="unsloth/Qwen3.5-0.8B-GGUF"
 llama-server \
-  -hf unsloth/Qwen3.5-2B-GGUF:UD-Q4_K_XL \
+  -hf unsloth/Qwen3.5-0.8B-GGUF:UD-Q4_K_XL \
   --ctx-size 16384 \
   --temp 0.7 \
   --top-p 0.8 \
   --top-k 20 \
   --min-p 0.00 \
-  --alias "unsloth/Qwen3.5-2B-GGUF" \
+  --alias "unsloth/Qwen3.5-0.8B-GGUF" \
   --port 8001
